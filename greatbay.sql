5ff8c6b047c04f14ca9030f4aa96534da26267a1
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-07 13:11:44 -0400

    created greatbay db with table

---
+++ greatbay.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE greatbay_db;
+
+USE greatbay_db;
+
+CREATE TABLE auctions(
+    id integer NOT NULL auto_increment,
+    item varchar(50) NOT NULL,
+    catergory varchar(50) NOT NULL,
+    startingBid integer DEFAULT0,
+    highestBid integer DEFAULT 0,
+    primary key (id)
+);
