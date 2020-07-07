4b09e66e42893ea2c201500071e1ef5879dcaed4
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-07 13:26:46 -0400

    Update greatbay.sql

--- greatbay.sql
+++ greatbay.sql
@@ -6,7 +6,7 @@ CREATE TABLE auctions(
     id integer NOT NULL auto_increment,
     item varchar(50) NOT NULL,
     catergory varchar(50) NOT NULL,
-    startingBid integer DEFAULT0,
+    startingBid integer DEFAULT 0,
     highestBid integer DEFAULT 0,
     primary key (id)
 );
